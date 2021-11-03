431254f3edcd4aaaf7a89cc2e2cec88dad32d5cc
jathorp <jackthorp07@gmail.com>
2021-11-03 01:58:04 -0400

    Added /rr-remove (reactrole-remove.js) It mostly works

--- commands/reactrole-remove.js
+++ commands/reactrole-remove.js
@@ -4,22 +4,13 @@ const {Connection} = require('mysql');
 
 module.exports = {
     data: new SlashCommandBuilder()
-            .setName('rr-add')
-            .setDescription('a generic command')
-            .addRoleOption(option => 
-                option.setName('role')
-                .setDescription('The role that will be given upon reacting')
-                .setRequired(true)
-            )
+            .setName('rr-remove')
+            .setDescription('Removes a reaction from the server\'s reactrole message')
             .addStringOption(option => 
                 option.setName('emoji')
-                .setDescription('The emoji that users will react to receive a role')
+                .setDescription('The emoji from the pair that will be removed')
                 .setRequired(true)
-            )
-            .addStringOption(option => 
-                option.setName('role-desc')
-                .setDescription('The title for this entry in the embed will be this option')
-                .setRequired(true)),
+            ),
 
     /**
      * 
@@ -30,14 +21,26 @@ module.exports = {
         
         // This might take longer than 3 seconds
         await interaction.deferReply();
-        
+        /**
+         * Steps to implement:
+         * 
+         * 1. Check for existence of RR message & pair to remove
+         * 
+         * 2. Fetch old message and make new embed as copy of old embed
+         * 
+         * 3. Remove field from embed with role & emoji
+         * 
+         * 4. Edit message (make sure to send new message object with new embed)
+         * 
+         * 5. Remove old pair from database
+         */
         // If there isn't a reactrole message we're in trouble
         console.log(`  Requesting reactrole message information...`)
         con.query(`SELECT * FROM messages WHERE messageName = "reactrole"`, (err, rows) => {
 
             if (err) {
                 console.log('  Error: A MySQL query error occured while fetching data from "messages" table');
-                console.log('  -> File: reactrole-add.js')
+                console.log('  -> File: reactrole-remove.js')
                 console.log(`    MySQL Error: ${err}`);
                 interaction.editReply({content: "Error: An error occured querying the database. Is it down?"});
                 return;
@@ -45,36 +48,32 @@ module.exports = {
 
             // If there's no reactrole message there's no need to continue
             if (rows.length == 0) {
-                interaction.editReply({content: "Error: No reactrole message exists, use /rr-create first!"});
+                interaction.editReply({content: "Error: No reactrole message exists, use /rr-create!"});
                 console.log('  Error: Reactrole message does not exist\n  Halting command')
                 return;
             }
             console.log(`  Reactrole message found`);
 
-            // Fetching all options since I need to use roleId in hte query and I want them all in the same place
-            let role = interaction.options.getRole('role');
+            // Fetching options and message params for fetching
             let emoji = interaction.options.getString('emoji');
-            let title = interaction.options.getString('role-desc');
-            // Used to resolve message
             let channel = rows[0].channelId;
             let message = rows[0].messageId;
 
             console.log(`  Requesting reactrole data...`)
-            con.query(`SELECT * FROM reactroles WHERE roleId="${role.id}" OR emoji="${emoji}"`, (err, rows) => {
+            con.query(`SELECT * FROM reactroles WHERE emoji="${emoji}"`, (err, rows) => {
 
                 if (err) {
                     console.log('  Error: A MySQL query error occured while fetching data from "reactroles" table');
-                    console.log('  -> File: reactrole-add.js')
+                    console.log('  -> File: reactrole-remove.js')
                     console.log(`    MySQL Error: ${err}`);
                     interaction.editReply({content: "Error: An error occured querying the database. Is it down?"});
                     return;
                 }
     
-                // If the query returned anything, stuff already exists with the emoji or role
-                // This would cause problems if it were allowed to continue because I'm too lazy to make it work
-                if (rows.length > 0) {
-                    interaction.editReply({content: "Error: Either the emoji or role requested is already in use in this reactrole menu\n(Duplicates aren't allowed)"});
-                    console.log('  Error: Duplicate emoji or role found\n  Halting command')
+                // If the query returns nothing, there is no pair wit hthe given emoji
+                if (rows.length == 0) {
+                    interaction.editReply({content: "Error: There is no existing pair with the given emoji"});
+                    console.log('  Error: Emoji not found\n  Halting command')
                     return;
                 }
                 console.log(`  Reactrole data validated`);
@@ -86,39 +85,50 @@ module.exports = {
                             interaction.editReply({content: 'Error: The database had bad data... Idk how that happened o-o'});
                             return;
                         } 
+
                         c.messages.fetch(message).then(m => {
-                            // Do work on the message here
-                            console.log('  Reactrole message fetched');
+                           
+                            // Do work on the message here 
                             let embed = m.embeds[0];
-                            embed.addField(`${title}`, `@${role.name} ==> ${emoji}`);
-                            m.edit({embeds:[embed]}).then(msg => {
-                                msg.react(emoji);
-                                console.log(`  Message reaction added`);
+                            console.log('  Message resolved');
+
+                            // Deleting the field containing the emoji
+                            for (let i = 0; i < embed.fields.length; i++) {
+                                if (embed.fields[i].value.includes(emoji)) {
+                                    embed.spliceFields(i, 1);
+                                    console.log('  Removed target field from message embed');
+                                }
+                            }
+                            m.edit({embeds:[embed]}).then(m => {
+                                try {
+                                    // God if this works this is such a roundabout route
+                                    let emId = m.guild.emojis.resolveId(emoji);
+                                    let key = m.reactions.cache.findKey(reaction => reaction.emoji.id == emId);
+                                    m.reactions.cache.delete(key);
+
+                                } catch (err) {
+                                    console.log(err);
+                                    console.log('  Yeah just delete this');
+                                    return;
+                                }
                             });
-                            console.log(`  Message edited`);
 
-                            // Time for more database queries!
-                            // JK
-                            // well, kinda, still gotta add new data
-                            con.query(`INSERT INTO reactroles(roleId, emoji) VALUES("${role.id}", "${emoji}")`, (err, rows) => {
-
+                            con.query(`DELETE FROM reactroles WHERE emoji="${emoji}"`, (err) => {
                                 if (err) {
-                                    console.log('  Error: A MySQL query error occured while inserting data into "reactroles" table');
-                                    console.log('  -> File: reactrole-add.js')
+                                    console.log('  Error: A MySQL query error occured while deleting from "reactroles" table');
+                                    console.log('  -> File: reactrole-remove.js')
                                     console.log(`    MySQL Error: ${err}`);
                                     interaction.editReply({content: "Error: An error occured querying the database. Is it down?"});
                                     return;
                                 }
-                                console.log('  Reactrole data updated');
-
-                                // Actually do something with the interaction
-                                // Then delete it to prevent clutter
-                                interaction.editReply({content: "It should be added!"}).then(reply => {
-                                    setTimeout(() => {
-                                        reply.delete();
-                                    }, 5000)
-                                })
                             });
+
+                            interaction.editReply({content: "The reaction should be removed"}).then(reply => {
+                                setTimeout(() => {
+                                    reply.delete();
+                                }, 5000)
+                            });
+
                         }).catch(err => {
                             // Error fetching message
                             console.log(`  Error: Fetching reactrole message failed`);
